8a837c376a03a63a9606b9f18026c53bfdcbc9a4
sberservice <madeinrussia@gmail.com>
2026-01-25 20:04:32 +0300

    fix: Update expected count for RULE_TYPES to reflect recent changes

--- tests/test_admin_panel_bot.py
+++ tests/test_admin_panel_bot.py
@@ -106,7 +106,7 @@ class TestRuleTypes(unittest.TestCase):
         """Test that RULE_TYPES has correct number of types."""
         from src.sbs_helper_telegram_bot.ticket_validator.admin_panel_bot_part import RULE_TYPES
         
-        self.assertEqual(len(RULE_TYPES), 5)
+        self.assertEqual(len(RULE_TYPES), 6)
 
 
 class TestConversationStates(unittest.TestCase):
